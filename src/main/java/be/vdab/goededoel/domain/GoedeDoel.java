782443289b4a88a90ef7cdf27f69a90a3c13e77c
Asterion <moonsovermowedmeadows@hotmail.com>
2019-06-17 18:34:19 +0200

    Javadoc

--- src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
@@ -1,17 +1,32 @@
 package be.vdab.goededoel.domain;
 
 import java.math.BigDecimal;
-
+/**
+ * Een <strong>goed doel</strong> waarvoor men geld inzamelt
+ * @author Joe Dalton
+ */
 public class GoedeDoel {
 	private final String naam;
 	private BigDecimal opgebracht = BigDecimal.ZERO;
 
+	/**
+	 * Maakt een GoedeDoel object
+	 * @param naam De naam van het goede doel
+	 */
 	public GoedeDoel(String naam) {
 		this.naam = naam;
 	}
+	/**
+	 * Geeft de naam terug
+	 * @return de naam 
+	 */
 	public String getNaam() {
 		return naam;
 	}
+	/**
+	 * Geeft terug hoeveel &euro; dit doel heeft opgebracht
+	 * @return het opgebracht bedrag
+	 */
 	public BigDecimal getOpgebracht() {
 		return opgebracht;
 	}
